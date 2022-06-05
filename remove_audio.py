a148be70b5b913b69470cfcaa08306e9a3252890
陈家齐 <alanjiach@gmail.com>
2022-06-05 13:16:22 -0500

    removed excess sample code

--- remove_audio.py
+++
@@ -1,8 +0,0 @@
-import ffmpeg
-import subprocess
-
-input = ffmpeg.input('source2.mp4')
-#audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
-video = input.video
-other_audio = ffmpeg.input('voice.mp3')
-ffmpeg.output(other_audio, video, 'video_with_other_audio.mp4').run()
